c851ac0a569a86a240c0e8befc02645d6c7d4529
NyiWaiYanHtoon <nwai39771@gmail.com>
2025-07-19 19:21:27 +0700

    fix version1.0

--- utils/get-video-duration.js
+++
@@ -1,7 +0,0 @@
-import { exec } from 'child_process';
-import util from 'util';
-const execAsync = util.promisify(exec);
-export async function getVideoDuration(filePath) {
-    const { stdout } = await execAsync(`ffprobe -v error -show_entries format=duration -of csv=p=0 "${filePath}"`);
-    return parseFloat(stdout);
-}
--- utils/validate-files.js
+++
@@ -1,4 +0,0 @@
-// Type guard helper to check if req.files is a fields object (not an array)
-export function isFilesFieldsObject(files) {
-    return !!files && !Array.isArray(files);
-}
