8f99bc0e7d68f5e17e3fa1423a9e7ec20502cfc6
NyiWaiYanHtoon <nwai39771@gmail.com>
2025-08-11 12:00:51 +0700

    debug 4

--- routes/kafka.ts
+++ routes/kafka.ts
@@ -5,11 +5,11 @@ import { ActionType } from "@prisma/client";
 
 const router = express.Router();
 
-// const TOPICS: { [key: string]: ActionType } = {
-//   "video-visit": "visit",
-//   "video-view": "view",
-//   "video-complete": "complete",
-// };
+const TOPICS: { [key: string]: ActionType } = {
+  "video-visit": "visit",
+  "video-view": "view",
+  "video-complete": "complete",
+};
 
 router.post("/produce", async (req, res) => {
   const { topic, data } = req.body;
@@ -17,12 +17,12 @@ router.post("/produce", async (req, res) => {
     return res.status(400).send("Something went wrong!");
   }
   try {
-    // const action= await insertAction(TOPICS[topic], data.videoId, data.userId)
-    // console.log("Action created: ", action);
-    await producer.send({
-      topic,
-      messages: [{ value: JSON.stringify(data) }],
-    });
+    const action= await insertAction(TOPICS[topic], data.videoId, data.userId)
+    console.log("Action created: ", action);
+    // await producer.send({
+    //   topic,
+    //   messages: [{ value: JSON.stringify(data) }],
+    // });
     res.status(200).json({ message: "Message sent" });
   } catch (err) {
     console.log("Kafka produce error:", err);
